d3281ce6b261558727e19bc8238588f003e40f13
Jay-R Joseph Gabunada <jayrjoseph@dnamicro.com>
2021-06-09 09:44:18 +0800

    added use-fetch code

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,40 @@
-import * as React from 'react';
+import LRU from 'lru-cache'
+import md5 from 'md5'
+import axios from 'axios'
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+const cache = new LRU(50)
+const defaultProcessResponse = (response: any) => response.data
+
+interface FetchOptions {
+  auth: {
+    username: string
+    password: string
+  }
+}
+type ProcessResponse = (response: any) => object
+
+interface FetchPromiseProps {
+  url: string
+  fetchOptions: FetchOptions
+  processResponse?: ProcessResponse
+}
+
+const useFetchPromise = ({ url, fetchOptions, processResponse = defaultProcessResponse }: FetchPromiseProps) => {
+  const key = `${url}.${md5(JSON.stringify(fetchOptions))}`
+  const value: any =  cache.get(key) || { status: 'new', data: null }
+
+  if (value.status === 'resolve') {
+    return value.data
+  }
+
+  const promise = axios.get(url, fetchOptions).then((response) => processResponse(response))
+  promise.then((data) => {
+    value.status = 'resolve'
+    value.data = data
+    cache.set(key, value)
+  })
+
+  throw promise
+}
+
+export default useFetchPromise
